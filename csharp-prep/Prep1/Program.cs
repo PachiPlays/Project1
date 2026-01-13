b7bef0790aefa1d9274503e89089c589e3b43705
William <willbob2490@gmail.com>
2026-01-13 14:10:53 -0700

    Changed code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("Please enter a number: ");
+        string user_input = Console.ReadLine();
+        Console.WriteLine($"The number you entered was {user_input}");
+
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.WriteLine($"What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+    
+
     }
 }
