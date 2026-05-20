70c30b9b65d7162a68d8326f3aa75c008b2422d3
William <willbob2490@gmail.com>
2026-05-20 17:58:40 -0600

    wrote code

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+
+        Square S1 = new Square("Brown", 5);
+        Circle C1 = new Circle("Green", 5);
+        Rectangle R1 = new Rectangle("Purple", 5, 4);
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(R1);
+        shapes.Add(C1);
+        shapes.Add(S1);
+
+        foreach (Shape shape in shapes)
+        {
+            double area4 = shape.GetArea();
+            string color4 = shape.GetColor();
+            Console.WriteLine(area4);
+            Console.WriteLine(color4);
+        }
+
+
+
+
     }
 }
